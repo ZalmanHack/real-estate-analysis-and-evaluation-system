2c8c27b1f7530ae8051e4c80daf2513c7d96dd44
zalmanHack <dobrenko44@gmail.com>
2020-04-18 18:10:42 +0300

    ver 0.0.3 Добавлена визуализация ответов НС

--- dnn.py
+++ dnn.py
@@ -1,14 +1,30 @@
+import os
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import QuantileTransformer
 from tensorflow import keras
+from datetime import datetime
 
+
+def create_test_matrix(x_range: list = (0, 100), y_range: list = (0, 100), resolution=100):
+    # создаем массив нужной нам размерности
+    array = np.zeros([resolution, resolution, 2])
+    # считаем шаг для X
+    x_step = abs((x_range[1] - x_range[0]) / resolution)
+    # считаем шаг для Y
+    y_step = abs((y_range[1] - y_range[0]) / resolution)
+    for xi in range(resolution):
+        for yi in range(resolution):
+            array[xi, yi] = np.array([x_range[0] + x_step*xi, y_range[0] + y_step*yi])
+    return array
 
 # загружаем данные
 def load_data(path):
     file = pd.read_csv(path, sep=';', encoding='utf-8-sig')
+    print(pd.DataFrame(file.loc[:, ['Широта', 'Долгота', 'Расстояние до набережной']]).to_numpy().max(axis=0))
     return pd.DataFrame(file.loc[:, ['Широта', 'Долгота', 'Расстояние до набережной']]).to_numpy()
 
 
@@ -35,15 +51,15 @@ def create_model(input_size=None):
     # первый слой ------------------------------------------------------------------------------------------------------
     model.add(keras.layers.Dense(128, input_dim=input_size, kernel_initializer='glorot_uniform'))
     model.add(keras.layers.BatchNormalization(epsilon=1e-6, center=True, scale=True))
-    model.add(keras.layers.Activation('elu'))
+    model.add(keras.layers.Activation('tanh'))
     # второй слой ------------------------------------------------------------------------------------------------------
     model.add(keras.layers.Dense(128, use_bias=True, kernel_initializer='glorot_uniform'))
     model.add(keras.layers.BatchNormalization(epsilon=1e-6, center=True, scale=True))
-    model.add(keras.layers.Activation('elu'))
+    model.add(keras.layers.Activation('tanh'))
     # третий слой ------------------------------------------------------------------------------------------------------
     model.add(keras.layers.Dense(128, use_bias=True, kernel_initializer='glorot_uniform'))
     model.add(keras.layers.BatchNormalization(epsilon=1e-6, center=True, scale=True))
-    model.add(keras.layers.Activation('elu'))
+    model.add(keras.layers.Activation('tanh'))
     # четвертый слой ---------------------------------------------------------------------------------------------------
     model.add(keras.layers.Dense(128, use_bias=True, kernel_initializer='glorot_uniform'))
     model.add(keras.layers.BatchNormalization(epsilon=1e-6, center=True, scale=True))
@@ -58,26 +74,36 @@ def create_model(input_size=None):
 
 def model_fit(model=None, X_train_norm=None, Y_train=None, batch_size=1, epochs=1, device='gpu'):
     assert model is not None and X_train_norm is not None and Y_train is not None
-    # TensorBoard
+    # TensorBoard ------------------------------------------------------------------------------------------------------
     callback_tb = keras.callbacks.TensorBoard(log_dir='dnn_log', histogram_freq=1, write_graph=True, write_images=True)
-    # Келбек сохраняющий веса модели
-    callback_as = keras.callbacks.ModelCheckpoint(filepath='last_training.okpt',
-                                                  save_weights_only=True, verbose=1, period=5)
-    # Келбек снижающий скорость обучения
+    # Келбек сохраняющий веса модели -----------------------------------------------------------------------------------
+    # Добавим эпоху в имя файла (uses `str.format`)
+    checkpoint_dir = 'weights {}'.format(datetime.today().strftime("%m.%d.%Y %H.%M"))
+    path = checkpoint_dir + "/cp-{epoch:04d}.ckpt"
+    callback_as = keras.callbacks.ModelCheckpoint(filepath=path, save_weights_only=True, verbose=1, period=5)
+    # Сохраним веса 1 эпохи, используя формат `checkpoint_path` format
+    model.save_weights(path.format(epoch=0))
+    # Келбек снижающий скорость обучения -------------------------------------------------------------------------------
     callback_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=10, min_lr=0.001)
     tf.device('/{}:0'.format(device))
     # заполнитель (обучение)
     model.fit(X_train_norm, Y_train, batch_size=batch_size, epochs=epochs, verbose=2, validation_split=0.2,
-                        callbacks=[callback_tb, callback_as, callback_lr])
+              callbacks=[callback_tb, callback_as, callback_lr])
     # сохранение модели
     model.save('last training model.h5')
     return model
 
 
+def get_last_dir():
+    list_dir = pd.DataFrame(os.listdir('.'))
+    list_dir = list_dir[list_dir[0].str.contains(r'weight')].sort_values(by=[0])
+    list_dir = list_dir.reset_index(drop=True)
+    return str(list_dir.iloc[-1, 0])
+
 
 def main():
     path = 'D:/Documents/учёба/КНТ/4 курс 2 семестр/dataset недвижимость СПБ/обработанные данные/' \
-           'КВАРТИРЫ Кронштадтский район.csv'
+           'ЗДАНИЯ.csv'
     data = load_data(path)
     # разбиваем данные на 2 выборки: тренировочную и тестовую
     train_set, test_set = train_test_split(data, test_size=0.2, random_state=42)
@@ -93,33 +119,67 @@ def main():
     print('Тестовая выборка:\n Входы: {}\n Выходы: {}\n'.format(X_test.shape, Y_test.shape))
     # нормализация данных
     X_train_norm = scale_data(X_train, X_train)
-    X_test_norm =  scale_data(X_train, X_test)
+    X_test_norm = scale_data(X_train, X_test)
     # Отрисовка данных
     show_data(X_train_norm)
 
-    t = input('Режим\n1 | Обучение\n2 | Загрузка ыесов\n3 | загрузка модели')
+    t = input('Режим\n1 | Обучение\n2 | Загрузка\n')
     if t == '1':
         # создаем нейронную сеть
         model = create_model(X_train_norm.shape[1])
         # обучаем модель
-        model = model_fit(model, X_train_norm, Y_train, 100, 200)
-    elif t == '2':
-        model = create_model(X_train_norm.shape[1])
-        model.load_weights('last_training.okpt')
+        model = model_fit(model, X_train_norm, Y_train, 20, 500)
     else:
-        model = keras.models.load_model('last training model.h5')
-
+        t = input('Режим\n1 | Загрузка весов\n2 | Загрузка модели\n')
+        if t == '1':
+            model = create_model(X_train_norm.shape[1])
+            lastest = tf.train.latest_checkpoint('weights 04.17.2020 20.52')
+            print(lastest)
+            model.load_weights(lastest)
+        else:
+            model = keras.models.load_model('last training model.h5')
+        t = input('Продолжить обучение?\n1 | Да\n2 | Нет\n')
+        if t == '1':
+            # обучаем модель
+            model = model_fit(model, X_train_norm, Y_train, 20, 500)
     model.summary()
-
     # оценка сети
     mse, mae = model.evaluate(X_test_norm, Y_test, verbose=0)
     print("Test mae: {}".format(mae))
-    # предсказание
-    pred = model.predict(X_test_norm)
-    print(pred[1555][0], Y_test[1555])
-    print(pred[1456][0], Y_test[1456])
-    print(pred[1741][0], Y_test[1741])
-    print(pred[354][0], Y_test[354])
+
+    # визуализация результатов обучения нейронной сети
+    # получем диапазон координат по X и Y
+    x_range = [X_test.min(axis=0)[0], X_test.max(axis=0)[0]]
+    y_range = [X_test.min(axis=0)[1], X_test.max(axis=0)[1]]
+    # создаем матрицу с координатами в ячейках
+    resolution = 1000
+    xy_matrix = create_test_matrix(x_range, y_range, resolution)
+    # нормализируем эти значения (построчно)
+    for i in range(xy_matrix.shape[0]):
+        xy_matrix[i] = scale_data(X_train, xy_matrix[i])
+    # создаем матрицу той же размерности, но с ответом НС в ячейках
+    predict_matrix = np.zeros([resolution, resolution])
+    # получем ответ от НС и заполняем матрицу
+    for i in range(xy_matrix.shape[0]):
+        predict_matrix[i] = model.predict(xy_matrix[i]).reshape(-1)
+    # отрисовываем полученый результат
+    plt.imshow(predict_matrix, interpolation='nearest')
+    plt.colorbar()
+    plt.show()
+
+    # # предсказание
+    # pred = model.predict(X_test_norm)
+    # summ = 0
+    # for i in range(1000):
+    #     print(((pred[i][0] - Y_test[i]) / Y_test[i]) * 100)
+    #     summ += ((pred[i][0] - Y_test[i]) / Y_test[i]) * 100
+    # print('{} %'.format(summ / 1000))
+    #
+    # c1 = tf.constant(1.0)
+    # second_graph = tf.Graph()
+    # with second_graph.as_default():
+    #     c2 = tf.constant(101.0)
+
 
 if __name__ == '__main__':
     main()
