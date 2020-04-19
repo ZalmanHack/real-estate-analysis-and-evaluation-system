0e2b5d9c6b9019cb60875d9cd49a96c6783e9ea8
zalmanHack <dobrenko44@gmail.com>
2020-04-19 07:00:40 +0300

    ver 0.0.4 Стабильная модель НС

--- dnn.py
+++ dnn.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import QuantileTransformer
+from sklearn.preprocessing import QuantileTransformer, MinMaxScaler
 from tensorflow import keras
 from datetime import datetime
+from mpldatacursor import datacursor
 
 
 def create_test_matrix(x_range: list = (0, 100), y_range: list = (0, 100), resolution=100):
@@ -16,17 +17,23 @@ def create_test_matrix(x_range: list = (0, 100), y_range: list = (0, 100), resol
     x_step = abs((x_range[1] - x_range[0]) / resolution)
     # считаем шаг для Y
     y_step = abs((y_range[1] - y_range[0]) / resolution)
-    for xi in range(resolution):
-        for yi in range(resolution):
-            array[xi, yi] = np.array([x_range[0] + x_step*xi, y_range[0] + y_step*yi])
+    for i in range(resolution):
+        for j in range(resolution):
+            array[i, j] = np.array([x_range[0] + x_step*i, y_range[0] + y_step*j])
+    array = np.flipud(array)
     return array
 
 # загружаем данные
 def load_data(path):
     file = pd.read_csv(path, sep=';', encoding='utf-8-sig')
-    print(pd.DataFrame(file.loc[:, ['Широта', 'Долгота', 'Расстояние до набережной']]).to_numpy().max(axis=0))
+    #print(pd.DataFrame(file.loc[:, ['Широта', 'Долгота', 'Расстояние до набережной']]).to_numpy().max(axis=0))
     return pd.DataFrame(file.loc[:, ['Широта', 'Долгота', 'Расстояние до набережной']]).to_numpy()
 
+
+
+def load_river(path):
+    file = pd.read_csv(path, sep=';', encoding='utf-8-sig')
+    return pd.DataFrame(file.loc[:, ['Широта', 'Долгота']]).to_numpy()
 
 # нормализация данных
 def scale_data(fit=None, transform=None):
@@ -36,12 +43,21 @@ def scale_data(fit=None, transform=None):
 
 
 # Отрисовка данных
-def show_data(data=None):
-    fig, ax = plt.subplots(1, 1, figsize=(12, 4))
-    ax.scatter(data[:, 0], data[:, 1], color='blue')
-    ax.set_title("After scaling (data)")
+def show_data(buildings=None, river=None, predict_matrix=None):
+    fig, ax = plt.subplots(1, 2, figsize=(12, 4))
+    ax[0].scatter(buildings[:, 1], buildings[:, 0], color='black', s=2)
+    ax[0].scatter(river[:, 1], river[:, 0], color='red')
+    ax[0].set_title("After scaling")
+    #im = ax[1].contourf(predict_matrix)
+    im = ax[1].imshow(predict_matrix, interpolation='nearest')
+    fig.colorbar(im, extend='both')
+    ax[1].set_title("predicted data")
+
+    datacursor(hover=False, bbox=dict(alpha=1, fc='w'), formatter='{z:.06g}'.format)
+
     plt.tight_layout()
     plt.show()
+
 
 
 # создаем нейронную сеть
@@ -49,30 +65,38 @@ def create_model(input_size=None):
     assert input_size is not None
     model = keras.Sequential()
     # первый слой ------------------------------------------------------------------------------------------------------
-    model.add(keras.layers.Dense(128, input_dim=input_size, kernel_initializer='glorot_uniform'))
+    model.add(keras.layers.Dense(256, input_dim=input_size, kernel_initializer='glorot_uniform'))
     model.add(keras.layers.BatchNormalization(epsilon=1e-6, center=True, scale=True))
-    model.add(keras.layers.Activation('tanh'))
+    model.add(keras.layers.Activation('relu'))
     # второй слой ------------------------------------------------------------------------------------------------------
     model.add(keras.layers.Dense(128, use_bias=True, kernel_initializer='glorot_uniform'))
     model.add(keras.layers.BatchNormalization(epsilon=1e-6, center=True, scale=True))
-    model.add(keras.layers.Activation('tanh'))
+    model.add(keras.layers.Activation('relu'))
     # третий слой ------------------------------------------------------------------------------------------------------
     model.add(keras.layers.Dense(128, use_bias=True, kernel_initializer='glorot_uniform'))
     model.add(keras.layers.BatchNormalization(epsilon=1e-6, center=True, scale=True))
-    model.add(keras.layers.Activation('tanh'))
+    model.add(keras.layers.Activation('elu'))
     # четвертый слой ---------------------------------------------------------------------------------------------------
     model.add(keras.layers.Dense(128, use_bias=True, kernel_initializer='glorot_uniform'))
     model.add(keras.layers.BatchNormalization(epsilon=1e-6, center=True, scale=True))
-    model.add(keras.layers.Activation('tanh'))
+    model.add(keras.layers.Activation('elu'))
+    # четвертый слой ---------------------------------------------------------------------------------------------------
+    model.add(keras.layers.Dense(128, use_bias=True, kernel_initializer='glorot_uniform'))
+    model.add(keras.layers.BatchNormalization(epsilon=1e-6, center=True, scale=True))
+    model.add(keras.layers.Activation('elu'))
     # model.add(keras.layers.Dropout(0.2))
-    # выходной слой ----------------------------------------------------------------------------------------------------
+    # четвертый слой ---------------------------------------------------------------------------------------------------
+    model.add(keras.layers.Dense(128, use_bias=True, kernel_initializer='glorot_uniform'))
+    model.add(keras.layers.BatchNormalization(epsilon=1e-6, center=True, scale=True))
+    model.add(keras.layers.Activation('elu'))
+    # четвертый слой ---------------------------------------------------------------------------------------------------
     model.add(keras.layers.Dense(1))
     # компилируем нейронную сеть
     model.compile(loss='mse', optimizer='adam', metrics=['mae'])
     return model
 
 
-def model_fit(model=None, X_train_norm=None, Y_train=None, batch_size=1, epochs=1, device='gpu'):
+def model_fit(model=None, X_train_norm=None, Y_train=None, batch_size=1, epochs=1, device='cpu'):
     assert model is not None and X_train_norm is not None and Y_train is not None
     # TensorBoard ------------------------------------------------------------------------------------------------------
     callback_tb = keras.callbacks.TensorBoard(log_dir='dnn_log', histogram_freq=1, write_graph=True, write_images=True)
@@ -84,10 +108,10 @@ def model_fit(model=None, X_train_norm=None, Y_train=None, batch_size=1, epochs=
     # Сохраним веса 1 эпохи, используя формат `checkpoint_path` format
     model.save_weights(path.format(epoch=0))
     # Келбек снижающий скорость обучения -------------------------------------------------------------------------------
-    callback_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=10, min_lr=0.001)
+    callback_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.0005)
     tf.device('/{}:0'.format(device))
     # заполнитель (обучение)
-    model.fit(X_train_norm, Y_train, batch_size=batch_size, epochs=epochs, verbose=2, validation_split=0.2,
+    model.fit(X_train_norm, Y_train, batch_size=batch_size, epochs=epochs, verbose=10, validation_split=0.2,
               callbacks=[callback_tb, callback_as, callback_lr])
     # сохранение модели
     model.save('last training model.h5')
@@ -120,20 +144,18 @@ def main():
     # нормализация данных
     X_train_norm = scale_data(X_train, X_train)
     X_test_norm = scale_data(X_train, X_test)
-    # Отрисовка данных
-    show_data(X_train_norm)
 
     t = input('Режим\n1 | Обучение\n2 | Загрузка\n')
     if t == '1':
         # создаем нейронную сеть
         model = create_model(X_train_norm.shape[1])
         # обучаем модель
-        model = model_fit(model, X_train_norm, Y_train, 20, 500)
+        model = model_fit(model, X_train_norm, Y_train, 40, 1000)
     else:
         t = input('Режим\n1 | Загрузка весов\n2 | Загрузка модели\n')
         if t == '1':
             model = create_model(X_train_norm.shape[1])
-            lastest = tf.train.latest_checkpoint('weights 04.17.2020 20.52')
+            lastest = tf.train.latest_checkpoint('weights 04.18.2020 21.45')  # weights 04.17.2020 20.52  weights 04.18.2020 19.46
             print(lastest)
             model.load_weights(lastest)
         else:
@@ -141,7 +163,7 @@ def main():
         t = input('Продолжить обучение?\n1 | Да\n2 | Нет\n')
         if t == '1':
             # обучаем модель
-            model = model_fit(model, X_train_norm, Y_train, 20, 500)
+            model = model_fit(model, X_train_norm, Y_train, 40, 1000)
     model.summary()
     # оценка сети
     mse, mae = model.evaluate(X_test_norm, Y_test, verbose=0)
@@ -151,8 +173,10 @@ def main():
     # получем диапазон координат по X и Y
     x_range = [X_test.min(axis=0)[0], X_test.max(axis=0)[0]]
     y_range = [X_test.min(axis=0)[1], X_test.max(axis=0)[1]]
+    print(x_range, y_range)
+    # return 0
     # создаем матрицу с координатами в ячейках
-    resolution = 1000
+    resolution = 500
     xy_matrix = create_test_matrix(x_range, y_range, resolution)
     # нормализируем эти значения (построчно)
     for i in range(xy_matrix.shape[0]):
@@ -162,24 +186,27 @@ def main():
     # получем ответ от НС и заполняем матрицу
     for i in range(xy_matrix.shape[0]):
         predict_matrix[i] = model.predict(xy_matrix[i]).reshape(-1)
+    # считываем файл с данными о расположении берегов
+    river_data = load_river('D:/Documents/учёба/КНТ/4 курс 2 семестр/dataset недвижимость СПБ/'
+                            'обработанные данные/НАБЕРЕЖНЫЕ.csv')
     # отрисовываем полученый результат
-    plt.imshow(predict_matrix, interpolation='nearest')
-    plt.colorbar()
-    plt.show()
+    show_data(X_train, river_data, predict_matrix)
 
-    # # предсказание
-    # pred = model.predict(X_test_norm)
-    # summ = 0
-    # for i in range(1000):
-    #     print(((pred[i][0] - Y_test[i]) / Y_test[i]) * 100)
-    #     summ += ((pred[i][0] - Y_test[i]) / Y_test[i]) * 100
-    # print('{} %'.format(summ / 1000))
-    #
-    # c1 = tf.constant(1.0)
-    # second_graph = tf.Graph()
-    # with second_graph.as_default():
-    #     c2 = tf.constant(101.0)
+    # предсказание
+    pred = model.predict(X_test_norm)
+    summ = 0
+    for i in range(Y_test.shape[0]):
+        # print(((pred[i][0] - Y_test[i]) / Y_test[i]) * 100)
+        summ += ((pred[i][0] - Y_test[i]) / Y_test[i]) * 100
+    print('{} %'.format(summ / Y_test.shape[0]))
 
+
+    pred = model.predict(X_train_norm)
+    summ = 0
+    for i in range(Y_train.shape[0]):
+        # print(((pred[i][0] - Y_test[i]) / Y_test[i]) * 100)
+        summ += ((pred[i][0] - Y_train[i]) / Y_train[i]) * 100
+    print('{} %'.format(summ / Y_train.shape[0]))
 
 if __name__ == '__main__':
     main()
